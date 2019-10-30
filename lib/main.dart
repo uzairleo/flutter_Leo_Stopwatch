3c76ddfe60a159597b8dbd589c834ebeec0fcac4
Uzair Leo <uzair.jan336@gmail.com>
2019-10-30 19:18:56 +0500

    add some comments too for understanding

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';//Allpackages
+
 
 void main() {
   runApp(new StopWatch());
